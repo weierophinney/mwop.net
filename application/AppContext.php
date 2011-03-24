438f23aab4bfd6ead9bc8c580572517c4481d9f2
Matthew Weier O'Phinney <matthew@zend.com>
2011-03-24 14:45:14 -0500

    Updated AppContext using new container generation
    - Updated config to remove case-sensitivity issues (MongoDB vs Mongodb
      for generated method names)
    - New container generation based on latest zf2-di status

--- application/AppContext.php
+++ application/AppContext.php
@@ -1,8 +1,8 @@
 <?php
 
-use Zend\Di\ServiceLocator;
+use Zend\Di\DependencyInjectionContainer;
 
-class AppContext extends ServiceLocator
+class AppContext extends DependencyInjectionContainer
 {
 
     public function get($name, array $params = array())
@@ -10,12 +10,43 @@ class AppContext extends ServiceLocator
         switch ($name) {
             case 'router':
             case 'mwop\Mvc\Router':
-                if (isset($this->services['mwop\Mvc\Router'])) {
-                    return $this->services['mwop\Mvc\Router'];
-                }
+                return $this->getMwopMvcRouter();
         
-                $object = new mwop\Mvc\Router();
-                $object->addRoutes(array (
+            case 'mongocxn':
+            case 'Mongo':
+                return $this->getMongo();
+        
+            case 'MongoDB':
+                return $this->getMongoDB();
+        
+            case 'mongo-collection-entries':
+            case 'MongoCollection':
+                return $this->getMongoCollection();
+        
+            case 'data-source':
+            case 'mwop\DataSource\Mongo':
+                return $this->getMwopDataSourceMongo();
+        
+            case 'resource-entry':
+            case 'mwop\Resource\EntryResource':
+                return $this->getMwopResourceEntryResource();
+        
+            case 'Blog\Controller\Entry':
+                return $this->getBlogControllerEntry();
+        
+            default:
+                return parent::get($name, $params);
+        }
+    }
+
+    public function getMwopMvcRouter()
+    {
+        if (isset($this->services['mwop\Mvc\Router'])) {
+            return $this->services['mwop\Mvc\Router'];
+        }
+        
+        $object = new mwop\Mvc\Router();
+        $object->addRoutes(array (
           'blog-create-form' => 
           array (
             'params' => 
@@ -33,74 +64,77 @@ class AppContext extends ServiceLocator
             ),
           ),
         ));
-                $this->services['mwop\Mvc\Router'] = $object;
-                return $object;
-        
-            case 'mongocxn':
-            case 'Mongo':
-                if (isset($this->services['Mongo'])) {
-                    return $this->services['Mongo'];
-                }
-        
-                $object = new Mongo();
-                $this->services['Mongo'] = $object;
-                return $object;
-        
-            case 'mongodb':
-            case 'MongoDB':
-                if (isset($this->services['MongoDB'])) {
-                    return $this->services['MongoDB'];
-                }
-        
-                $object = new MongoDB($this->get('mongocxn'), 'mwoptest');
-                $this->services['MongoDB'] = $object;
-                return $object;
-        
-            case 'mongo-collection-entries':
-            case 'MongoCollection':
-                if (isset($this->services['MongoCollection'])) {
-                    return $this->services['MongoCollection'];
-                }
-        
-                $object = new MongoCollection($this->get('mongodb'), 'entries');
-                $this->services['MongoCollection'] = $object;
-                return $object;
-        
-            case 'data-source':
-            case 'mwop\DataSource\Mongo':
-                if (isset($this->services['mwop\DataSource\Mongo'])) {
-                    return $this->services['mwop\DataSource\Mongo'];
-                }
-        
-                $object = new mwop\DataSource\Mongo($this->get('mongo-collection-entries'));
-                $this->services['mwop\DataSource\Mongo'] = $object;
-                return $object;
-        
-            case 'resource-entry':
-            case 'mwop\Resource\EntryResource':
-                if (isset($this->services['mwop\Resource\EntryResource'])) {
-                    return $this->services['mwop\Resource\EntryResource'];
-                }
-        
-                $object = new mwop\Resource\EntryResource();
-                $object->setDataSource($this->get('data-source'));
-                $object->setCollectionClass('mwop\\Resource\\MongoCollection');
-                $this->services['mwop\Resource\EntryResource'] = $object;
-                return $object;
-        
-            case 'Blog\Controller\Entry':
-                if (isset($this->services['Blog\Controller\Entry'])) {
-                    return $this->services['Blog\Controller\Entry'];
-                }
-        
-                $object = new Blog\Controller\Entry();
-                $object->resource($this->get('resource-entry'));
-                $this->services['Blog\Controller\Entry'] = $object;
-                return $object;
-        
-            default:
-                return parent::get($name, $params);
+        $this->services['mwop\Mvc\Router'] = $object;
+        return $object;
+    }
+
+    public function getMongo()
+    {
+        if (isset($this->services['Mongo'])) {
+            return $this->services['Mongo'];
         }
+        
+        $object = new Mongo();
+        $this->services['Mongo'] = $object;
+        return $object;
+    }
+
+    public function getMongoDB()
+    {
+        if (isset($this->services['MongoDB'])) {
+            return $this->services['MongoDB'];
+        }
+        
+        $object = new MongoDB($this->getMongocxn(), 'mwoptest');
+        $this->services['MongoDB'] = $object;
+        return $object;
+    }
+
+    public function getMongoCollection()
+    {
+        if (isset($this->services['MongoCollection'])) {
+            return $this->services['MongoCollection'];
+        }
+        
+        $object = new MongoCollection($this->getMongoDB(), 'entries');
+        $this->services['MongoCollection'] = $object;
+        return $object;
+    }
+
+    public function getMwopDataSourceMongo()
+    {
+        if (isset($this->services['mwop\DataSource\Mongo'])) {
+            return $this->services['mwop\DataSource\Mongo'];
+        }
+        
+        $object = new mwop\DataSource\Mongo($this->getMongoCollectionEntries());
+        $this->services['mwop\DataSource\Mongo'] = $object;
+        return $object;
+    }
+
+    public function getMwopResourceEntryResource()
+    {
+        if (isset($this->services['mwop\Resource\EntryResource'])) {
+            return $this->services['mwop\Resource\EntryResource'];
+        }
+        
+        $object = new mwop\Resource\EntryResource();
+        $object->setDataSource($this->getDataSource());
+        $object->setCollectionClass('mwop\\Resource\\MongoCollection');
+        $this->services['mwop\Resource\EntryResource'] = $object;
+        return $object;
+    }
+
+    public function getBlogControllerEntry()
+    {
+        if (isset($this->services['Blog\Controller\Entry'])) {
+            return $this->services['Blog\Controller\Entry'];
+        }
+        
+        $object = new Blog\Controller\Entry();
+        $object->resource($this->getResourceEntry());
+        $this->services['Blog\Controller\Entry'] = $object;
+        return $object;
     }
 
     public function getRouter()
@@ -111,11 +145,6 @@ class AppContext extends ServiceLocator
     public function getMongocxn()
     {
         return $this->get('Mongo');
-    }
-
-    public function getMongodb()
-    {
-        return $this->get('MongoDB');
     }
 
     public function getMongoCollectionEntries()
