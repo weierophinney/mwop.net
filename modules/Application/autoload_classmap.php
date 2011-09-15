ddf23256999621bea08701c0c4c0d5db400c7b7d
Matthew Weier O'Phinney <matthew@zend.com>
2011-09-14 22:05:47 -0500

    Restructured Application module to mirror recommendations

--- modules/Application/autoload_classmap.php
+++ modules/Application/autoload_classmap.php
@@ -1,5 +1,5 @@
 <?php
 return array (
-  'Application\\Controller\\PageController' => __DIR__ . DIRECTORY_SEPARATOR . '.' . DIRECTORY_SEPARATOR . 'Controller' . DIRECTORY_SEPARATOR . 'PageController.php',
+  'Application\\Controller\\PageController' => __DIR__ . DIRECTORY_SEPARATOR . '.' . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR . 'Application' . DIRECTORY_SEPARATOR . 'Controller' . DIRECTORY_SEPARATOR . 'PageController.php',
   'Application\\Module' => __DIR__ . DIRECTORY_SEPARATOR . '.' . DIRECTORY_SEPARATOR . 'Module.php',
 );
