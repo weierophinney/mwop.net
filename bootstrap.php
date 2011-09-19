73427c07e9eb9b60bf56b095766e796b1038752b
Matthew Weier O'Phinney <matthew@zend.com>
2011-09-18 20:56:31 -0500

    Moved bulk of bootstrapping logic into separate file
    - Will allow re-use by scripts

---
+++ bootstrap.php
@@ -0,0 +1,45 @@
+<?php
+ini_set("display_errors", true);
+error_reporting(E_ALL | E_STRICT);
+
+// Define application environment
+defined('APPLICATION_ENV')
+    || define('APPLICATION_ENV', (getenv('APPLICATION_ENV') ? getenv('APPLICATION_ENV') : 'development'));
+
+// Define application path
+defined('APPLICATION_PATH')
+    || define('APPLICATION_PATH', (getenv('APPLICATION_PATH') ? getenv('APPLICATION_PATH') : realpath(__DIR__)));
+
+// Ensure library/ is on include_path
+set_include_path(implode(PATH_SEPARATOR, array(
+    '.',
+    '/home/matthew/git/zf2/library',
+    get_include_path(),
+)));
+
+require_once 'Zend/Loader/AutoloaderFactory.php';
+Zend\Loader\AutoloaderFactory::factory(array(
+    'Zend\Loader\ClassMapAutoloader' => array(
+        __DIR__ . '/modules/Zf2Module/classmap.php',
+    ),
+    'Zend\Loader\StandardAutoloader' => array(),
+));
+// Init config
+$appConfig = include __DIR__ . '/configs/application.config.php';
+
+/**
+ * Long-hand:
+ * $modules = new Zf2Module\ModuleManager;
+ * $modules->getLoader()->registerPaths($appConfig->modulePaths->toArray());
+ * $modules->loadModules($appConfig->modules->toArray());
+ */
+$modules = Zf2Module\ModuleManager::fromConfig($appConfig);
+
+// Get the merged config object
+$config = $modules->getMergedConfig();
+
+// Create application, bootstrap, and run
+$bootstrap = new $config->bootstrap_class($config);
+$application = new Zf2Mvc\Application;
+$bootstrap->bootstrap($application);
+
