494a43e53449a664a318a58f28e3c86e26c37f95
Matthew Weier O'Phinney <matthew@zend.com>
2012-03-20 22:41:24 -0500

    Created logic for compiling entries by criteria
    - As each entry is discovered, we add to a number of SortedEntries
      properties, so that when we want to paginate, we do not need to do any
      filtering/sorting of the entries.
    - Added body for compileTagCloud -- though likely needs to create markup
      and write to a file
    - Started logic for compilePaginatedEntriesByTag()

--- module/Blog/src/Blog/Compiler.php
+++ module/Blog/src/Blog/Compiler.php
@@ -1,13 +1,21 @@
 <?php
 namespace Blog;
 
-use Iterator,
+use DateTime,
+    DateTimezone,
+    Iterator,
     Zend\Paginator\Paginator,
-    Zend\Paginator\Adapter\Iterator as IteratorPaginator;
+    Zend\Paginator\Adapter\Iterator as IteratorPaginator,
+    Zend\Tag\Cloud as TagCloud;
 
 
 class Compiler
 {
+    protected $byAuthor;
+    protected $byDay;
+    protected $byMonth;
+    protected $byTag;
+    protected $byYear;
     protected $entries;
     protected $files;
 
@@ -18,7 +26,7 @@ class Compiler
 
     public function compileEntryViewScripts()
     {
-        $entries = $this->getEntries();
+        $entries = $this->prepareEntries();
 
         foreach ($entries as $entry) {
             // We need:
@@ -30,7 +38,7 @@ class Compiler
 
     public function compilePaginatedEntries()
     {
-        $entries   = $this->getEntries();
+        $entries   = $this->prepareEntries();
         $paginator = $this->getPaginator($entries);
 
         // We need:
@@ -56,7 +64,7 @@ class Compiler
 
     public function compilePaginatedEntriesByDate()
     {
-        $entries   = $this->getEntries();
+        $entries   = $this->prepareEntries();
 
         // We need:
         // - How many entries to include per page
@@ -94,7 +102,7 @@ class Compiler
 
     public function compilePaginatedEntriesByTag()
     {
-        $entries   = $this->getEntries();
+        $entries   = $this->prepareEntries();
 
         // We need:
         // - How many entries to include per page
@@ -106,47 +114,133 @@ class Compiler
         //   - Does this mean a "renderer"?
         //
         // Then: 
-        // - Filter entries by tag
-        //   {
-        //      zf:  [...]
-        //      php: [...]
-        //   }
         // - loop from page 1 to last page for each criteria
         // - Generate each page
+
+        $tags = array();
+        foreach ($this->byTag as $tag => $list) {
+
+// What's in this loop will be the same for all the compile methods.
+// Only difference will be what template is used, and what file is written to
+            
+            // Get a paginator for this tag
+            $paginator = $this->getPaginator($list);
+
+            // Loop through pages
+            $pageCount = count($paginator);
+            for ($i = 1; $i <= $pageCount; $i++) {
+                $paginator->setCurrentPageNumber($i);
+
+                // Generate this page
+            }
+        }
     }
 
-    public function compileTagCloud()
+    /**
+     * Compile a tag cloud from the entries
+     *
+     * 
+     * @todo   Should this write the tag cloud markup to a file?
+     * @param  string $tagUrlTemplate 
+     * @param  array $cloudOptions 
+     * @return TagCloud
+     */
+    public function compileTagCloud($tagUrlTemplate = '/blog/tag/%s', $cloudOptions = array())
     {
-        $entries = $this->getEntries();
+        $this->prepareEntries();
 
-        // We need:
-        // - Location at which to write view scripts
-        // - Template for view script
-        //   - Does this mean a "renderer"?
-        // Then:
-        // - Foreach entry:
-        //   - Get tags
-        //     - Create new "tag" key if needed
-        //     - Increment count for tag by 1
-        // - Generate tag cloud
+        $tags = array();
+        foreach ($this->byTag as $tag => $list) {
+            $tags[] = array(
+                'title'   => $tag,
+                'weight'  => count($list),
+                'params'  => array(
+                    'url' => sprintf($tagUrlTemplate, $tag),
+                ),
+            );
+        }
+        $options['tags'] = $tags;
+
+        return new TagCloud($options);
     }
 
-    protected function getEntries()
+    /**
+     * Prepare the list of entries
+     * 
+     * Loops through the filesystem tree, looking for PHP files
+     * that return EntryEntity objects. For each returned, adds it
+     * to:
+     *
+     * - $entries, which has all entries
+     * - $byYear, a hash of year/SortedEntries pairs
+     * - $byMonth, a hash of year-month/SortedEntries pairs
+     * - $byDay, a hash of year-month-day/SortedEntries pairs
+     * - $byTag, a hash of tag/SortedEntries pairs
+     * - $byAuthor, a hash of author/SortedEntries pairs
+     *
+     * @return void
+     */
+    protected function prepareEntries()
     {
         if ($this->entries) {
             return $this->entries;
         }
 
-        $entries = new Compiler\SortedEntries();
+        $this->entries  = new Compiler\SortedEntries();
+        $this->byYear   = array();
+        $this->byMonth  = array();
+        $this->byDay    = array();
+        $this->byTag    = array();
+        $this->byAuthor = array();
         foreach ($this->files as $file) {
             $entry = include $file->getRealPath();
             if (!$entry instanceof EntryEntity) {
                 continue;
             }
-            $entries->insert($entry, $entry->getCreated());
+
+            // First, set in entries
+            $timestamp = $entry->getCreated();
+            $this->entries->insert($entry, $created);
+
+            // Then, set in appropriate year, month, and day slots
+            $date      = new DateTime();
+            $date->setTimestamp($timestamp)
+                 ->setTimezone(new DateTimezone($entry->getTimezone()));
+
+            $year  = $date->format('Y');
+            $month = $date->format('Y-m');
+            $day   = $date->format('Y-m-d');
+
+            if (!isset($this->byYear[$year])) {
+                $this->byYear[$year] = new Compiler\SortedEntries();
+            }
+            $this->byYear[$year]->insert($entry, $created);
+
+            if (!isset($this->byMonth[$month])) {
+                $this->byMonth[$month] = new Compiler\SortedEntries();
+            }
+            $this->byMonth[$month]->insert($entry, $created);
+
+            if (!isset($this->byDay[$day])) {
+                $this->byDay[$day] = new Compiler\SortedEntries();
+            }
+            $this->byDay[$day]->insert($entry, $created);
+
+            // Next, set in appropriate tag lists
+            foreach ($entry->getTags() as $tag) {
+                if (!isset($this->byTag[$tag])) {
+                    $this->byTag[$tag] = new Compiler\SortedEntries();
+                }
+                $this->byTag[$tag]->insert($entry, $created);
+            }
+
+            // Finally, by author
+            $author = $entry->getAuthor();
+            if (!isset($this->byAuthor[$author])) {
+                $this->byAuthor[$author] = new Compiler\SortedEntries();
+            }
+            $this->byAuthor[$author]->insert($entry, $created);
         }
-        $this->entries = $entries;
-        return $this->entries;
     }
 
     /**
