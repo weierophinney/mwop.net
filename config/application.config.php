bd7f8979b33f07121f543eaaf2ce49595e9a661b
Matthew Weier O'Phinney <matthew@zend.com>
2012-04-02 11:15:06 -0500

    Removed Cache module
    - Benchmarking wasn't giving good results
    - Not really necessary anymore with static blog

--- config/application.config.php
+++ config/application.config.php
@@ -2,7 +2,6 @@
 return array(
     'modules' => array(
         'Application',
-        // 'Cache', // commenting out as profiling shows increased load times?
         'PhlyCommon',
         'PhlyBlog',
         'PhlyComic',
--- module/Cache/autoload_classmap.php
+++
@@ -1,4 +0,0 @@
-<?php
-return array(
-    'Cache\Listener' => __DIR__ . '/src/Cache/Listener.php',
-);
