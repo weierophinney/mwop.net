cd6e49067d489b70b91d4ad19ec734c11555da9a
Matthew Weier O'Phinney <matthew@zend.com>
2012-06-24 12:21:48 -0500

    Better listening logic
    - Simply look for touched file, and remove if discovered.
    - Fail if unable to remove the file (as this will cause repeated deployment
      attempts)

--- bin/deploy-listener.pl
+++ bin/deploy-listener.pl
@@ -1,16 +1,16 @@
 #!/usr/bin/perl
 # Deployment listener
 #
-# Checks /var/local/mwop.net.update every 30s to see if it contains the value
-# "1". If so, it executes the deployment script.
+# Checks /var/local/mwop.net.update every 30s to see if it exists;
+# if so, it executes the deployment script.
 #
 # Logs are written for:
-# - Failure to open the update file
+# - Failure to remove the update file
 # - Successful deployment
 # - Deployment failure
 #
-# If the script is unable to open the update file to read or write 10 or more
-# times, the daemon will halt.
+# If the script is unable to remove the update file, the daemon 
+# will halt
 
 use strict;
 use warnings;
@@ -24,42 +24,22 @@ $SIG{TERM} = sub { $continue = 0 };
 my $updateFile   = "/var/local/mwop.net.update";
 my $updateScript = "/home/matthew/bin/deploy-mwop";
 my $logFile      = "/var/local/mwop.net-deploy.log";
-my $failureCount = 0;
-my $maxFailures  = 10;
 while ($continue) {
-    my $flag;
-    my $read;
-    my $write;
-
     # 30s intervals between iterations
     sleep 30;
 
-    if (!open($read, "<", $updateFile)) {
-        system('echo "' . time() . ': Failed to read ' . $updateFile . '" >> ' .  $logFile);
-        $failureCount += 1;
-        if ($failureCount >= $maxFailures) {
-            $continue = 0;
-        }
-        next;
-    }
-    read $read, $flag, 1;
-    close($read);
-
-    if ($flag != "1") {
+    # Check for update file, and restart loop if not found
+    unless (-e $updateFile) {
         next;
     }
 
-    # Disable updating (so later processes don't try and update)
-    if (!open($write, ">", $updateFile)) {
-        system('echo "' . time() . ': Failed to UPDATE ' . $updateFile . '" >> ' . $logFile);
-        $failureCount += 1;
-        if ($failureCount >= $maxFailures) {
-            $continue = 0;
-        }
+    # Remove update file
+    unless (unlink($updateFile) == 0) {
+        # If unable to unlink, we need to quit
+        system('echo "' . time() . ': Failed to REMOVE ' . $updateFile . '" >> ' . $logFile);
+        $continue = 0;
         next;
     }
-    print $write, "0";
-    close($write);
 
     # Deploy
     system($updateScript);
