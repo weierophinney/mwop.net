cf84b7e5556a2c33766fab989fd13e0d388ff7f4
Matthew Weier O'Phinney <matthew@zend.com>
2012-12-12 16:53:57 -0600

    Added page caching configuration
    - Added page caching configuration

--- config/autoload/global.php
+++ config/autoload/global.php
@@ -5,9 +5,23 @@ return array(
         'comic_file'      => '%s.html',
         'all_comics_file' => 'comics.html',
     ),
+    'phly-simple-page' => array(
+        'cache' => array(
+            'adapter' => array(
+                'name' => 'filesystem',
+                'options' => array(
+                    'namespace' => 'pages',
+                    'cache_dir' => getcwd() . '/data/pages',
+                ),
+            ),
+        ),
+    ),
     'service_manager' => array(
         'invokables' => array(
             'Zend\Session\SessionManager' => 'Zend\Session\SessionManager',
+        ),
+        'factories' => array(
+            'PhlySimplePage\PageCache' => 'PhlySimplePage\PageCacheService',
         ),
     ),
 );
