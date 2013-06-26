f5167d497cdcc259206628c58b58971b95d6eaa2
Matthew Weier O'Phinney <matthew@zend.com>
2013-06-26 13:49:18 -0500

    Simplify
    - Remove ALL the bash!

--- deploy/before_symlink.rb
+++ deploy/before_symlink.rb
@@ -1,7 +1,6 @@
-run "(cd #{release_path} ; mkdir tmp)"
-run! "(cd #{release_path}/tmp && exec ssh-agent bash -c 'ssh-add /home/deploy/.ssh/mwopnet-deploy-key && git clone -b mwop.net.config git@github.com:weierophinney/site-settings.git')"
-run "(cd #{release_path} && mv tmp/site-settings/*.php config/autoload/)"
+run! "exec ssh-agent bash -c 'ssh-add /home/deploy/.ssh/mwopnet-deploy-key && git clone -b mwop.net.config git@github.com:weierophinney/site-settings.git #{current_path}/tmp/config')"
+run "mv #{current_path}/tmp/config/*.php #{release_path}/config/autoload/)"
 
 run "ln -nfs #{shared_path}/users.db #{release_path}/data/users.db"
-run "(cd #{release_path} && php public/index.php githubfeed fetch)"
-run "(cd #{release_path} && php public/index.php phlysimplepage cache clear all)"
+run "cd #{release_path} && php public/index.php githubfeed fetch"
+run "cd #{release_path} && php public/index.php phlysimplepage cache clear all"
