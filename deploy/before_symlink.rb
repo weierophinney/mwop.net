61f36d3aac851c6f4979adec3eaa538536daf6d3
Matthew Weier O'Phinney <matthew@zend.com>
2013-06-26 13:42:19 -0500

    Another hook fix
    - Forgot to close a quote
    - Removed unnecessary bash-isms

--- deploy/before_symlink.rb
+++ deploy/before_symlink.rb
@@ -1,5 +1,5 @@
 run "(cd #{release_path} ; mkdir tmp)"
-run! "(cd #{release_path}/tmp && $(exec ssh-agent bash -c 'ssh-add /home/deploy/.ssh/mwopnet-deploy-key && git clone -b mwop.net.config git@github.com:weierophinney/site-settings.git))"
+run! "(cd #{release_path}/tmp && exec ssh-agent bash -c 'ssh-add /home/deploy/.ssh/mwopnet-deploy-key && git clone -b mwop.net.config git@github.com:weierophinney/site-settings.git')"
 run "(cd #{release_path} && mv tmp/site-settings/*.php config/autoload/)"
 
 run "ln -nfs #{shared_path}/users.db #{release_path}/data/users.db"
