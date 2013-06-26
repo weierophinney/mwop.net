680fc3c4216fc47f468d286886a119844fd55826
Matthew Weier O'Phinney <matthew@zend.com>
2013-06-26 14:07:04 -0500

    Minor deploy fixes
    - Fix bashism
    - use full path to php

--- deploy/before_symlink.rb
+++ deploy/before_symlink.rb
@@ -1,4 +1,4 @@
-run "cp #{shared_path}/appconfig/*.php #{release_path}/config/autoload/)"
+run "cp #{shared_path}/appconfig/*.php #{release_path}/config/autoload/"
 run "ln -nfs #{shared_path}/users.db #{release_path}/data/users.db"
-run "cd #{release_path} && php public/index.php githubfeed fetch"
-run "cd #{release_path} && php public/index.php phlysimplepage cache clear all"
+run "cd #{release_path} && /usr/bin/php public/index.php githubfeed fetch"
+run "cd #{release_path} && /usr/bin/php public/index.php phlysimplepage cache clear all"
