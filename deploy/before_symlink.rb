ada4c724d83bfb98914662f3a0176f68c701b2c0
Matthew Weier O'Phinney <matthew@zend.com>
2013-06-26 14:50:21 -0500

    Fetch comics before deploy

--- deploy/before_symlink.rb
+++ deploy/before_symlink.rb
@@ -1,4 +1,5 @@
 run "cp #{shared_path}/appconfig/*.php #{release_path}/config/autoload/"
 run "ln -nfs #{shared_path}/users.db #{release_path}/data/users.db"
 run "cd #{release_path} && /usr/bin/php public/index.php githubfeed fetch"
+run "cd #{release_path} && /usr/bin/php public/index.php phlycomic fetch all"
 run "cd #{release_path} && /usr/bin/php public/index.php phlysimplepage cache clear all"
