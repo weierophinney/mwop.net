57a974b1a590d40a1b72cf144fc482bc770c1a1c
Matthew Weier O'Phinney <matthew@zend.com>
2013-12-03 09:58:58 -0600

    Update index.php to make usage with built-in web server easier
    - Ported from ZF2 skeleton app

--- public/index.php
+++ public/index.php
@@ -1,3 +1,8 @@
 <?php
+// Decline static file requests back to the PHP built-in webserver
+if (php_sapi_name() === 'cli-server' && is_file(__DIR__ . parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH))) {
+    return false;
+}
+
 $application = include __DIR__ . '/../bootstrap.php';
 $application->run();
