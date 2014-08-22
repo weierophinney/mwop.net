a5f8c9ff2ff1b44fa0515c5d11b8aca27762102e
Matthew Weier O'Phinney <matthew@zend.com>
2014-08-22 13:33:20 -0500

    Schedule job queue jobs after activation

--- zpk/scripts/post_activate.php
+++ zpk/scripts/post_activate.php
@@ -1,9 +1,8 @@
 <?php
-$baseDir = getenv('ZS_APPLICATION_BASE_DIR');
-
-// Place a marker file indicating the application directory so cronjobs know 
-// where to operate
-if (!is_dir('/usr/local/zend/tmp/mwop.net')) {
-    mkdir('/usr/local/zend/tmp/mwop.net', 0777, true);
-}
-file_put_contents('/usr/local/zend/tmp/mwop.net/path', $baseDir);
+$queue = new ZendJobQueue();
+$queue->createHttpJob('/jobs/comics.php', null, [
+    'schedule' => '0 5 * * *', // every day, at 5 AM
+]);
+$queue->createHttpJob('/jobs/github-feed.php', null, [
+    'schedule' => '5,20,35,40 * * * *', // every 15 minutes
+]);
