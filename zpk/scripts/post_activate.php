a59b9d9176db6aa55b8097b89a631c39cc1379fe
Matthew Weier O'Phinney <matthew@zend.com>
2014-08-22 13:44:11 -0500

    Use fully qualified URI for HTTP jobs
    - Otherwise, zend server fails to deploy!

--- zpk/scripts/post_activate.php
+++ zpk/scripts/post_activate.php
@@ -1,8 +1,8 @@
 <?php
 $queue = new ZendJobQueue();
-$queue->createHttpJob('/jobs/comics.php', [], [
+$queue->createHttpJob('http://staging.mwop.net/jobs/comics.php', [], [
     'schedule' => '0 5 * * *', // every day, at 5 AM
 ]);
-$queue->createHttpJob('/jobs/github-feed.php', [], [
+$queue->createHttpJob('http://staging.mwop.net/jobs/github-feed.php', [], [
     'schedule' => '5,20,35,40 * * * *', // every 15 minutes
 ]);
