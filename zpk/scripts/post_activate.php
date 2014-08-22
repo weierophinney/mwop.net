1c58bc5addefc73405db93502b008e5ca7cc6e3e
Matthew Weier O'Phinney <matthew@zend.com>
2014-08-22 13:38:31 -0500

    Fix job scheduling
    - Contrary to all examples... second parameter MUST be an array.

--- zpk/scripts/post_activate.php
+++ zpk/scripts/post_activate.php
@@ -1,8 +1,8 @@
 <?php
 $queue = new ZendJobQueue();
-$queue->createHttpJob('/jobs/comics.php', null, [
+$queue->createHttpJob('/jobs/comics.php', [], [
     'schedule' => '0 5 * * *', // every day, at 5 AM
 ]);
-$queue->createHttpJob('/jobs/github-feed.php', null, [
+$queue->createHttpJob('/jobs/github-feed.php', [], [
     'schedule' => '5,20,35,40 * * * *', // every 15 minutes
 ]);
