317088e8d3406a9490d8405ca823383f11bbb207
Matthew Weier O'Phinney <matthew@zend.com>
2014-08-22 13:48:58 -0500

    Better schedule for comics job
    - UTC time for job should be 1000

--- zpk/scripts/post_activate.php
+++ zpk/scripts/post_activate.php
@@ -1,7 +1,7 @@
 <?php
 $queue = new ZendJobQueue();
 $queue->createHttpJob('http://staging.mwop.net/jobs/comics.php', [], [
-    'schedule' => '0 5 * * *', // every day, at 5 AM
+    'schedule' => '0 10 * * *', // every day, at 5 AM America/Chicago (server is in UTC)
 ]);
 $queue->createHttpJob('http://staging.mwop.net/jobs/github-feed.php', [], [
     'schedule' => '5,20,35,40 * * * *', // every 15 minutes
