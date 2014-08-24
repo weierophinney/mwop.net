1248f494827e42a45290e952534980ac5fa477b2
Matthew Weier O'Phinney <matthew@zend.com>
2014-08-24 12:14:05 -0500

    Cleaned up cache clearing script
    - Make it easier to add more hosts
    - Don't use a temp variable for the URL; unnecessary

--- public/jobs/clear-cache.php
+++ public/jobs/clear-cache.php
@@ -6,19 +6,22 @@ if (! ZendJobQueue::getCurrentJobId()) {
     exit(1);
 }
 
-$hosts = ['mwop.net', 'www.mwop.net', 'staging.mwop.net'];
+$hosts = [
+    'mwop.net',
+    'www.mwop.net',
+    'staging.mwop.net',
+];
+
 $rules = [
     'mwop_home'   => '/',
     'mwop_resume' => '/resume',
 ];
 
 foreach ($hosts as $host) {
-    $baseUrl = 'http://' . $host;
-
     foreach ($rules as $rule => $path) {
         page_cache_remove_cached_contents_by_uri(
             $rule,
-            $baseUrl . $path
+            'http://' . $host . $path
         );
     }
 }
