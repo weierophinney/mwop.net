07871394cdb9c89bb07d6d4ae20efbf37e28411a
Matthew Weier O'Phinney <matthew@zend.com>
2014-08-26 16:22:49 -0500

    Updated previous tips to link to this one

--- data/posts/2014/2014-09-02-ZendServerDeployment-Part-3.php
+++ data/posts/2014/2014-09-02-ZendServerDeployment-Part-3.php
@@ -87,7 +87,10 @@ system($command);
     <li><a href="/blog/2014-08-11-zend-server-deployment-part-1.html">Tip 1: zf-deploy</a></li>
     <li><a href="/blog/2014-08-28-zend-server-deployment-part-2.html">Tip 2: Recurring Jobs</a></li>
     <li><a href="/blog/2014-09-04-zend-server-deployment-part-4.html">Tip 4: Secure your job scripts</a></li>
+<<<<<<< HEAD
     <li><a href="/blog/2014-09-09-zend-server-deployment-part-5.html">Tip 5: Set your job status</a></li>
+=======
+>>>>>>> Updated previous tips to link to this one
 </ul
 
 <p>
