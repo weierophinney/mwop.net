f766d6cd853a13cd76e5c7ca7fa38291a00cabf0
Matthew Weier O'Phinney <matthew@zend.com>
2014-08-26 17:11:21 -0500

    Updated previous tips to link to this one

--- data/posts/2014/2014-09-02-ZendServerDeployment-Part-3.php
+++ data/posts/2014/2014-09-02-ZendServerDeployment-Part-3.php
@@ -16,7 +16,7 @@ $entry->setAuthor($author);
 $entry->setDraft(false);
 $entry->setPublic(true);
 $entry->setCreated(new \DateTime('2014-09-02 08:30', new \DateTimezone('America/Chicago')));
-$entry->setUpdated(new \DateTime('2014-09-11 08:30', new \DateTimezone('America/Chicago')));
+$entry->setUpdated(new \DateTime('2014-09-16 08:30', new \DateTimezone('America/Chicago')));
 $entry->setTimezone('America/Chicago');
 $entry->setTags(array(
   'apigility',
@@ -89,6 +89,7 @@ system($command);
     <li><a href="/blog/2014-09-04-zend-server-deployment-part-4.html">Tip 4: Secure your job scripts</a></li>
     <li><a href="/blog/2014-09-09-zend-server-deployment-part-5.html">Tip 5: Set your job status</a></li>
     <li><a href="/blog/2014-09-11-zend-server-deployment-part-6.html">Tip 6: Page caching</a></li>
+    <li><a href="/blog/2014-09-16-zend-server-deployment-part-7.html">Tip 7: zs-client</a></li>
 </ul
 
 <p>
