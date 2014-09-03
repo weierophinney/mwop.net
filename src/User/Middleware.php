8b6126b1a31efcc0a6e72f667ea243c7da9f807a
Matthew Weier O'Phinney <matthew@zend.com>
2014-09-03 16:37:50 -0500

    Do not pipe Auth middleware to `/`
    Doing so means it will be called without an authentication service type!

--- src/User/Middleware.php
+++ src/User/Middleware.php
@@ -8,7 +8,6 @@ class Middleware extends BaseMiddleware
     public function __construct(Auth $auth, AuthCallback $callback, Logout $logout)
     {
         parent::__construct();
-        $this->pipe('/', $auth);
         $this->pipe('/github/oauth2callback', $callback);
         $this->pipe('/google/oauth2callback', $callback);
         $this->pipe('/twitter/oauth_callback', $callback);
