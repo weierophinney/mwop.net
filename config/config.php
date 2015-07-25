0db6c5b75d92abbc39908e30c4128723fcad7763
Matthew Weier O'Phinney <matthew@zend.com>
2015-07-25 16:10:12 -0500

    Added "modules" key to config
    - Done to trick zf-deploy into thinking this is a ZF2 application.

--- config/config.php
+++ config/config.php
@@ -1,13 +1,6 @@
 <?php
 use Zend\Config\Factory as Config;
 
-/**
- * Trick zf-deploy into thinking this is a ZF2 app so it can build a package.
- *
- * 'modules' => array(
- * )
- */
-
 return Config::fromFiles(
     glob('config/autoload/{,*.}{global,local}.php', GLOB_BRACE)
 );
