831b0929ba857e10de0100a0a55c59a8521ca017
Matthew Weier O'Phinney <matthew@zend.com>
2015-09-02 12:27:10 -0500

    Updated to phly-mustache 2.0
    - Removed custom aggregate, namespaced resolvers; these are now part of 2.0.
    - Updated logic in MustacheTemplate to account for 2.0 API changes (with regards
      to getting the default resolver, and adding paths).
    - Updated the MustacheTemplateFactory to add pragmas per the new API, and to add
      the ContextualEscape pragma (should I choose to use it later).
    
    Integration testing shows everything working!

--- src/Template/MustacheTemplate.php
+++ src/Template/MustacheTemplate.php
@@ -2,7 +2,8 @@
 namespace Mwop\Template;
 
 use Phly\Mustache\Mustache;
-use Phly\Mustache\Resolver\ResolverInterface;
+use Phly\Mustache\Resolver\AggregateResolver;
+use Phly\Mustache\Resolver\DefaultResolver;
 use SplStack;
 use Zend\Expressive\Template\TemplateInterface;
 use Zend\Expressive\Template\TemplatePath;
@@ -15,7 +16,7 @@ class MustacheTemplate implements TemplateInterface
     private $renderer;
 
     /**
-     * @var AggregateMustacheResolver
+     * @var AggregateResolver
      */
     private $resolver;
 
@@ -23,7 +24,7 @@ class MustacheTemplate implements TemplateInterface
      * Constructor.
      *
      * Composes the Mustache instance. If the Mustache instance does not compose
-     * an AggregateMustacheResolver, one is created, the original resolver is
+     * an AggregateResolver, one is created, the original resolver is
      * attached to it, and the aggregate is pushed into the Mustache instance;
      * this is done to ensure namespaced template paths work correctly.
      */
@@ -33,15 +34,10 @@ class MustacheTemplate implements TemplateInterface
 
         $resolver = $renderer->getResolver();
 
-        if (! $resolver instanceof AggregateMustacheResolver) {
-            $aggregate = $this->createDefaultResolver();
-            $aggregate->attach($resolver);
-            $renderer->setResolver($aggregate);
-            $resolver = $aggregate;
-        }
-
-        if (! $this->aggregateHasNamespacedResolver($resolver)) {
-            $this->injectNamespacedResolver($resolver);
+        if (! $resolver->hasType(DefaultResolver::class)) {
+            $resolver->attach($this->createDefaultResolver(), 0);
+        } else {
+            $this->extractDefaultResolver($resolver);
         }
     }
 
@@ -58,7 +54,7 @@ class MustacheTemplate implements TemplateInterface
      */
     public function addPath($path, $namespace = null)
     {
-        $this->resolver->setTemplatePath($path, $namespace);
+        $this->resolver->addTemplatePath($path, $namespace);
     }
 
     /**
@@ -70,7 +66,7 @@ class MustacheTemplate implements TemplateInterface
         $paths    = [];
 
         foreach ($resolver->getNamespaces() as $namespace) {
-            $namespace = ($namespace !== NamespacedMustacheResolver::DEFAULT_NAMESPACE) ? $namespace : null;
+            $namespace = ($namespace !== DefaultResolver::DEFAULT_NAMESPACE) ? $namespace : null;
             foreach ($resolver->getTemplatePath($namespace) as $path) {
                 $paths[] = new TemplatePath($path, $namespace);
             }
@@ -80,56 +76,35 @@ class MustacheTemplate implements TemplateInterface
     }
 
     /**
-     * Creates and returns an AggregateMustacheResolver composing a NamespacedMustacheResolver.
+     * Creates and returns a DefaultResolver.
      *
-     * NamespacedMustacheResolver is set at -1 priority, to ensure it resolves
-     * later than any composed originally.
-     *
-     * @return AggregateMustacheResolver
+     * @return DefaultResolver
      */
     private function createDefaultResolver()
     {
-        $resolver = new AggregateMustacheResolver();
-        $this->injectNamespacedResolver($resolver);
-        return $resolver;
+        $this->resolver = new DefaultResolver();
+        return $this->resolver;
     }
 
     /**
-     * Does the aggregate compose a namespaced resolver?
-     *
-     * If it does, it sets the internal $resolver property to the first found.
-     *
-     * If the internal $resolver property is already set, returns early.
-     *
-     * @param AggregateMustacheResolver $aggregate
-     * @return bool
-     */
-    private function aggregateHasNamespacedResolver(AggregateMustacheResolver $aggregate)
-    {
-        if ($this->resolver) {
-            return true;
-        }
-
-        foreach ($aggregate as $resolver) {
-            if ($resolver instanceof NamespacedMustacheResolver) {
-                $this->resolver = $resolver;
-                return true;
-            }
-        }
-
-        return false;
-    }
-
-    /**
-     * Inject the aggregate with a namespaced resolver.
+     * Extract and compose the DefaultResolver found in an AggregateResolver.
      *
      * Also sets the internal $resolver property to the first found.
      *
-     * @param AggregateMustacheResolver $aggregate
+     * @param AggregateResolver $aggregate
      */
-    private function injectNamespacedResolver(AggregateMustacheResolver $aggregate)
+    private function extractDefaultResolver(AggregateResolver $aggregate)
     {
-        $this->resolver = new NamespacedMustacheResolver();
-        $aggregate->attach($this->resolver, -1);
+        if ($this->resolver instanceof DefaultResolver) {
+            return;
+        }
+
+        $resolver = $aggregate->fetchByType(DefaultResolver::class);
+        if ($resolver instanceof AggregateResolver) {
+            $queue    = $resolver->getIterator();
+            $resolver = $queue->top();
+        }
+
+        $this->resolver = $resolver;
     }
 }
