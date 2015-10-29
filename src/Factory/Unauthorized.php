a7882b83f9a32cb877755e54963ef0d643362fc7
Matthew Weier O'Phinney <matthew@zend.com>
2015-10-29 08:39:03 -0500

    Ensure "unauthorized" views have URI helper
    - Updated Unauthorized middleware to accept a router instance, and inject it in
      a PageView before passing to the template renderer.

--- src/Factory/Unauthorized.php
+++ src/Factory/Unauthorized.php
@@ -2,11 +2,16 @@
 namespace Mwop\Factory;
 
 use Mwop\Unauthorized as Middleware;
+use Zend\Expressive\Router\RouterInterface;
+use Zend\Expressive\Template\TemplateRendererInterface;
 
 class Unauthorized
 {
     public function __invoke($services)
     {
-        return new Middleware($services->get('Zend\Expressive\Template\TemplateRendererInterface'));
+        return new Middleware(
+            $services->get(TemplateRendererInterface::class),
+            $services->get(RouterInterface::class)
+        );
     }
 }
--- src/Unauthorized.php
+++ src/Unauthorized.php
@@ -2,16 +2,22 @@
 namespace Mwop;
 
 use Zend\Diactoros\Response\HtmlResponse;
+use Zend\Expressive\Router\RouterInterface;
 use Zend\Expressive\Template\TemplateRendererInterface;
 
 class Unauthorized
 {
     private $renderer;
+    private $router;
     private $template;
 
-    public function __construct(TemplateRendererInterface $renderer, $template = 'error::401')
-    {
+    public function __construct(
+        TemplateRendererInterface $renderer,
+        RouterInterface $router,
+        $template = 'error::401'
+    ) {
         $this->renderer = $renderer;
+        $this->router   = $router;
         $this->template = $template;
     }
 
@@ -22,10 +28,11 @@ class Unauthorized
         }
 
         $new = $req->getUri()->withPath('/auth');
-        $view = [
+        $view = new PageView([
             'auth_path' => (string) $new,
             'redirect'  => (string) $req->getOriginalRequest()->getUri(),
-        ];
+        ]);
+        $view->setRouter($this->router);
 
         return new HtmlResponse(
             $this->renderer->render($this->template, $view),
