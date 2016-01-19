11b00bb9b127df0fa131af2cbd4bda7575778f9b
Matthew Weier O'Phinney <matthew@zend.com>
2016-01-19 17:35:08 -0600

    Temporarily disable auth

--- src/ComicsPage.php
+++ src/ComicsPage.php
@@ -5,9 +5,11 @@ class ComicsPage extends Page
 {
     public function __invoke($request, $response, $next)
     {
+        /*
         if (! $request->getAttribute('user', false)) {
             return $next($request, $response->withStatus(401), 401);
         }
+         */
 
         return parent::__invoke($request, $response, $next);
     }
