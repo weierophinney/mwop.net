03eeb6aace9d46e9e38910ac8fa379e43033faf1
Matthew Weier O'Phinney <matthew@zend.com>
2016-06-29 08:52:54 -0500

    Created cloud deploy configuration
    Does the following:
    
    - Stops nginx and php7.0-fpm
    - Installs dependencies:
      - Installs nginx, php, and npm
      - Downloads and installs composer, if not already present; otherwise, updates it.
      - Installs nginx configuration
      - Installs php-fpm configuration
      - Installs grunt globally
    - Installs the app:
      - Runs composer install
      - Runs all scripts
      - Runs npm install and grunt
    - Restarts php-fpm, nginx, and crontab

---
+++ .aws/install_dependencies.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+SCRIPT_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# Install needed dependencies
+apt-get install -y nginx php7.0 php7.0-bcmath php7.0-bz2 php7.0-cli php7.0-ctype php7.0-curl php7.0-dom php7.0-fileinfo php7.0-fpm php7.0-gd php7.0-iconv php7.0-intl php7.0-json php7.0-mbstring php7.0-pdo php7.0-pdo-sqlite php7.0-phar php7.0-readline php7.0-simplexml php7.0-sockets php7.0-sqlite3 php7.0-tidy php7.0-tokenizer php7.0-xml php7.0-xsl php7.0-xmlreader php7.0-xmlwriter php7.0-zip npm
+
+# Get Composer, and install to /usr/local/bin
+if [ ! -f "/usr/local/bin/composer" ];then
+    php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+    php -r "if (hash_file('SHA384', 'composer-setup.php') === 'e115a8dc7871f15d853148a7fbac7da27d6c0030b848d9b3dc09e2a0388afed865e6a3d6b3c0fad45c48e2b5fc1196ae') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+    php composer-setup.php --install-dir=/usr/local/bin --filename=composer
+    php -r "unlink('composer-setup.php');"
+else
+    /usr/local/bin/composer self-update --stable --no-ansi --no-interaction
+fi
+
+# Copy nginx configuration
+cp ${SCRIPT_PATH}/mwop.net.conf /etc/nginx/sites-available/
+if [ ! -e "/etc/nginx/sites-enabled/mwop.net.conf" ];then
+    (cd /etc/nginx/sites-enabled && ln -s ../sites-available/mwop.net.conf .)
+fi
+
+# Copy php configuration
+cp ${SCRIPT_PATH}/php.ini /etc/php/7.0/fpm/conf.d mwop.ini
+cp ${SCRIPT_PATH}/php-fpm.conf /etc/php/7.0/fpm/pool.d/www.conf
+
+# Make a log directory for php-fpm
+if [ ! -d "/var/log/php" ];then
+    mkdir -p /var/log/php
+fi
+chown -R www-data.www-data /var/log/php
+chmod -R ug+rwX /var/log/php
+
+# Install grunt globally
+npm install -g grunt-cli
---
+++ .aws/stage.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+composer install --no-ansi --no-dev --no-interaction --no-progress --no-scripts --optimize-autoloader
+php bin/mwop.net.php seed-blog-db
+php bin/mwop.net.php tag-cloud
+php bin/mwop.net.php feed-generator
+php bin/mwop.net.php cache-posts
+php bin/mwop.net.php github-links
+php vendor/bin/phly-comic.php fetch-all --output=data/comics.mustache
+npm install
+grunt
+rm -Rf node_modules
+chmod -R ug+rwX data
