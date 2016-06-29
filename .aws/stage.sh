f7f5f0d589f3b8c0171521f0ae3fe621df08dbc9
Matthew Weier O'Phinney <matthew@zend.com>
2016-06-29 14:06:49 -0500

    Ensure that staging script executes in the install directory

--- .aws/stage.sh
+++ .aws/stage.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
-cp /var/www/config/php/*.* config/autoload/
-COMPOSER_HOME=/var/cache/composer composer install --no-ansi --no-dev --no-interaction --no-progress --no-scripts --optimize-autoloader
-php bin/mwop.net.php seed-blog-db
-php bin/mwop.net.php tag-cloud
-php bin/mwop.net.php feed-generator
-php bin/mwop.net.php cache-posts
-php bin/mwop.net.php github-links
-php vendor/bin/phly-comic.php fetch-all --output=data/comics.mustache
-npm install
-grunt
-rm -Rf node_modules
-chmod -R ug+rwX data
+(
+    cd /var/www/mwop.net ;
+    cp /var/www/config/php/*.* config/autoload/ ;
+    COMPOSER_HOME=/var/cache/composer composer install --no-ansi --no-dev --no-interaction --no-progress --no-scripts --optimize-autoloader ;
+    php bin/mwop.net.php seed-blog-db ;
+    php bin/mwop.net.php tag-cloud ;
+    php bin/mwop.net.php feed-generator ;
+    php bin/mwop.net.php cache-posts ;
+    php bin/mwop.net.php github-links ;
+    php vendor/bin/phly-comic.php fetch-all --output=data/comics.mustache ;
+    npm install ;
+    grunt ;
+    rm -Rf node_modules ;
+    chmod -R ug+rwX data ;
+)
