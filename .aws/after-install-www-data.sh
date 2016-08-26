1ba52fe9bca9c5eaf5d73798b9e81c34ea6e20ee
Matthew Weier O'Phinney <matthew@zend.com>
2016-08-26 13:46:35 -0500

    Install composer in two passes
    - First pass is unoptimized, so that we get dev deps, which then allow us to run
      things like the prep-offline-pages script.
    - Second pass optimizes for production.
    - Wrap these in `$()` constructs, to ensure `&&` works as expected.

--- .aws/after-install-www-data.sh
+++ .aws/after-install-www-data.sh
@@ -10,9 +10,9 @@
     echo "Syncing production configuration" ;
     cp /var/www/config/php/*.* config/autoload/ ;
 
-    # Execute a composer installation
-    echo "Executing composer" ;
-    COMPOSER_HOME=/var/cache/composer && composer install --quiet --no-ansi --no-dev --no-interaction --no-progress --no-scripts --no-plugins --optimize-autoloader ;
+    # Execute a composer installation ; do a full install at first
+    echo "Executing composer (dev)" ;
+    $(COMPOSER_HOME=/var/cache/composer && composer install --quiet --no-ansi --no-interaction --no-progress --no-scripts --no-plugins) ;
 
     # Setting mwop.net.php permissions
     echo "Setting mwop.net.php permissions" ;
@@ -21,6 +21,10 @@
     # Running build process
     echo "Running build process" ;
     composer build ;
+
+    # After the build, we optimize the installation
+    echo "Executing composer (prod)" ;
+    $(COMPOSER_HOME=/var/cache/composer && composer install --quiet --no-ansi --no-dev --no-interaction --no-progress --no-scripts --no-plugins --optimize-autoloader) ;
 )
 
 echo "[DONE] after-install-www-data.sh"
