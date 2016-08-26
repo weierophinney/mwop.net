bd497e9c8ec650d69320086c7319d01ea304e8b2
Matthew Weier O'Phinney <matthew@zend.com>
2016-08-26 13:57:21 -0500

    Added error handling to build script
    The build script errored, but the error did not break the deployment as it
    should have; my suspicion is the hard-coded `exit 0` in the script.
    
    This patch updates the script to wrap several calls into `if` statements, in
    order to catch cases where an exit code > 0 occurs, and, when detected, exit
    with 1 in order to fail the build.

--- .aws/after-install-www-data.sh
+++ .aws/after-install-www-data.sh
@@ -12,7 +12,10 @@
 
     # Execute a composer installation ; do a full install at first
     echo "Executing composer (dev)" ;
-    $(COMPOSER_HOME=/var/cache/composer && composer install --quiet --no-ansi --no-interaction --no-progress --no-scripts --no-plugins) ;
+    if ! $(COMPOSER_HOME=/var/cache/composer && composer install --quiet --no-ansi --no-interaction --no-progress --no-scripts --no-plugins); then
+        echo "[FAILED] Failed performing composer dev install" ;
+        exit 1 ;
+    fi
 
     # Setting mwop.net.php permissions
     echo "Setting mwop.net.php permissions" ;
@@ -20,12 +23,23 @@
 
     # Running build process
     echo "Running build process" ;
-    composer build ;
+    if ! $(composer build); then
+        echo "[FAILED] Failed building application" ;
+        exit 1 ;
+    fi
 
     # After the build, we optimize the installation
     echo "Executing composer (prod)" ;
-    $(COMPOSER_HOME=/var/cache/composer && composer install --quiet --no-ansi --no-dev --no-interaction --no-progress --no-scripts --no-plugins --optimize-autoloader) ;
+    if ! $(COMPOSER_HOME=/var/cache/composer && composer install --quiet --no-ansi --no-dev --no-interaction --no-progress --no-scripts --no-plugins --optimize-autoloader); then
+        echo "[FAILED] Failed performing composer production install" ;
+        exit 1 ;
+    fi
 )
+
+if [ $? -ne 0 ] ; then
+    echo "[FAILED] One or more build tasks failed" ;
+    exit 1 ;
+fi
 
 echo "[DONE] after-install-www-data.sh"
 exit 0
