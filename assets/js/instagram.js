d95ba32c8059d455f1e97550327e6b29ffdbc58d
Matthew Weier O'Phinney <matthew@weierophinney.net>
2021-03-11 11:55:04 -0600

    fix: fix issue with link generation in IG modals

--- assets/js/instagram.js
+++ assets/js/instagram.js
@@ -5,7 +5,7 @@
     .on('show.bs.modal', function(event) {
       let image = $(event.relatedTarget).data('instagram-image');
       let url   = $(event.relatedTarget).data('instagram-url');
-      let html  = '<a href"' + url + '"><img class="img-fluid" src="' + image + '"></a><br /><em>Click image to see post on Instagram</em>';
+      let html  = '<a href="' + url + '"><img class="img-fluid" src="' + image + '"></a><br /><em>Click image to see post on Instagram</em>';
       $(this).find('.modal-body').html(html);
     });
 })(jQuery);
