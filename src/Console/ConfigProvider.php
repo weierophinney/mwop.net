9744c78577cd6532b08c10a60869639813531bbc
Matthew Weier O'Phinney <matthew@weierophinney.net>
2021-03-11 13:16:01 -0600

    feat: remove logic duplication around comic fetching
    There was a mis-match between the deployment config and the cronjob.
    This patch adds a new command that proxies to the phly-comic `FetchAllComics` command, passing it a list of comics to exclude, as well as other options.
    Doing so allows calling one command consistently without arguments everywhere.

--- src/Console/ConfigProvider.php
+++ src/Console/ConfigProvider.php
@@ -9,12 +9,17 @@ declare(strict_types=1);
 
 namespace Mwop\Console;
 
+use PhlyComic\Console\FetchAllComics;
+use PhlyComic\Console\FetchComic;
+use PhlyComic\Console\ListComics;
+
 class ConfigProvider
 {
     public function __invoke(): array
     {
         return [
             'dependencies' => $this->getDependencies(),
+            'laminas-cli'  => $this->getConsoleConfig(),
         ];
     }
 
@@ -22,10 +27,26 @@ class ConfigProvider
     {
         return [
             'invokables' => [
-                ClearCache::class => ClearCache::class,
+                ClearCache::class         => ClearCache::class,
+                FetchAllComics::class     => FetchAllComics::class,
+                FetchComic::class         => FetchComic::class,
+                FetchComicsCommand::class => FetchComicsCommand::class,
+                ListComics::class         => ListComics::class,
             ],
             'factories'  => [
                 FeedAggregator::class => FeedAggregatorFactory::class,
+            ],
+        ];
+    }
+
+    public function getConsoleConfig(): array
+    {
+        return [
+            'commands' => [
+                'comics:list'      => ListComics::class,
+                'comics:fetch'     => FetchComic::class,
+                'comics:fetch-all' => FetchAllComics::class,
+                'comics:for-site'  => FetchComicsCommand::class,
             ],
         ];
     }
---
+++ src/Console/FetchComicsCommand.php
@@ -0,0 +1,50 @@
+<?php
+
+/**
+ * @copyright Copyright (c) Matthew Weier O'Phinney
+ * @license http://opensource.org/licenses/BSD-2-Clause BSD-2-Clause
+ */
+
+declare(strict_types=1);
+
+namespace Mwop\Console;
+
+use PhlyComic\Console\FetchAllComics;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\ArrayInput;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class FetchComicsCommand extends Command
+{
+    protected static $defaultName = 'comics:for-site';
+
+    protected function configure(): void
+    {
+        $this->setDescription('Fetch comics for the website.');
+        $this->setHelp('Delegates to the comics:fetch-all command, with a list of exclusions.');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        /** @var FetchAllComics $fetchAllCommand */
+        $fetchAllCommand = $this->getApplication()->find('comics:fetch-all');
+
+        return $fetchAllCommand->run(
+            new ArrayInput([
+                '--exclude' => [
+                    'bloom-county',
+                    'dilbert',
+                    'g-g',
+                    'listen-tome',
+                    'nih',
+                    'phd',
+                    'reptilis-rex',
+                ],
+                '--output'    => 'data/comics.phtml',
+                '--processes' => 5,
+            ]),
+            $output
+        );
+    }
+}
