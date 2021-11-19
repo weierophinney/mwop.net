81b6b381c7e121587ae217c97ebe240597f77140
Matthew Weier O'Phinney <matthew@weierophinney.net>
2021-11-19 10:56:31 -0600

    fix: use $_SERVER, not $_ENV
    The environment variables are evidently coming in as $_SERVER, not $_ENV (and not accessible via getenv()).

--- config/autoload/zzz-env.global.php
+++ config/autoload/zzz-env.global.php
@@ -10,8 +10,10 @@ use Mwop\Blog\Listener\CacheBlogPostListener;
 use Mwop\Contact\Listener\SendContactMessageListener;
 use Psr\Log\LoggerInterface;
 
+error_log(var_export($_SERVER, true), 4);
+
 /** @var string $messageToAddress */
-$messageToAddress = $_ENV['CONTACT_MESSAGE_TO_ADDRESS'] ?? '';
+$messageToAddress = $_SERVER['CONTACT_MESSAGE_TO_ADDRESS'] ?? '';
 
 /**
  * Defines env-specific settings.
@@ -19,7 +21,7 @@ $messageToAddress = $_ENV['CONTACT_MESSAGE_TO_ADDRESS'] ?? '';
 return [
     'blog'         => [
         'api'     => [
-            'key' => $_ENV['BLOG_API_KEY'] ?? '',
+            'key' => $_SERVER['BLOG_API_KEY'] ?? '',
         ],
         'disqus'  => [
             'key' => 'phlyboyphly',
@@ -28,10 +30,10 @@ return [
             'enabled' => true,
         ],
         'twitter' => [
-            'consumer_key'        => $_ENV['TWITTER_CONSUMER_KEY'] ?? '',
-            'consumer_secret'     => $_ENV['TWITTER_CONSUMER_SECRET'] ?? '',
-            'access_token'        => $_ENV['TWITTER_ACCESS_TOKEN'] ?? '',
-            'access_token_secret' => $_ENV['TWITTER_ACCESS_TOKEN_SECRET'] ?? '',
+            'consumer_key'        => $_SERVER['TWITTER_CONSUMER_KEY'] ?? '',
+            'consumer_secret'     => $_SERVER['TWITTER_CONSUMER_SECRET'] ?? '',
+            'access_token'        => $_SERVER['TWITTER_ACCESS_TOKEN'] ?? '',
+            'access_token_secret' => $_SERVER['TWITTER_ACCESS_TOKEN_SECRET'] ?? '',
         ],
     ],
     'cache'        => [
@@ -40,13 +42,13 @@ return [
         ],
     ],
     'contact'      => [
-        'recaptcha_pub_key'  => $_ENV['RECAPTCHA_PUB_KEY'] ?? '',
-        'recaptcha_priv_key' => $_ENV['RECAPTCHA_PRIV_KEY'] ?? '',
+        'recaptcha_pub_key'  => $_SERVER['RECAPTCHA_PUB_KEY'] ?? '',
+        'recaptcha_priv_key' => $_SERVER['RECAPTCHA_PRIV_KEY'] ?? '',
         'message'            => [
             'to'     => $messageToAddress,
             'from'   => null,
             'sender' => [
-                'address' => $_ENV['CONTACT_MESSAGE_SENDER_ADDRESS'] ?? '',
+                'address' => $_SERVER['CONTACT_MESSAGE_SENDER_ADDRESS'] ?? '',
             ],
         ],
     ],
@@ -69,27 +71,27 @@ return [
     'mail'         => [
         'transport' => [
             'class'    => Swift_SmtpTransport::class,
-            'host'     => $_ENV['MAIL_TRANSPORT_HOST'] ?? '',
-            'port'     => $_ENV['MAIL_TRANSPORT_PORT'] ?? '',
+            'host'     => $_SERVER['MAIL_TRANSPORT_HOST'] ?? '',
+            'port'     => $_SERVER['MAIL_TRANSPORT_PORT'] ?? '',
             'ssl'      => 'tls',
-            'username' => $_ENV['MAIL_TRANSPORT_USERNAME'] ?? '',
-            'password' => $_ENV['MAIL_TRANSPORT_PASSWORD'] ?? '',
+            'username' => $_SERVER['MAIL_TRANSPORT_USERNAME'] ?? '',
+            'password' => $_SERVER['MAIL_TRANSPORT_PASSWORD'] ?? '',
         ],
     ],
     /*
     'oauth2'        => [
         'debug'  => [],
         'github' => [
-            'clientId'     => $_ENV['OAUTH2_GITHUB_CLIENTID'],
-            'clientSecret' => $_ENV['OAUTH2_GITHUB_CLIENTSECRET'],
-            'redirectUri'  => $_ENV['OAUTH2_GITHUB_REDIRECTURI'],
+            'clientId'     => $_SERVER['OAUTH2_GITHUB_CLIENTID'],
+            'clientSecret' => $_SERVER['OAUTH2_GITHUB_CLIENTSECRET'],
+            'redirectUri'  => $_SERVER['OAUTH2_GITHUB_REDIRECTURI'],
         ],
         'google' => [
-            'clientId'     => $_ENV['OAUTH2_GOOGLE_CLIENTID'],
-            'clientSecret' => $_ENV['OAUTH2_GOOGLE_CLIENTSECRET'],
-            'redirectUri'  => $_ENV['OAUTH2_GOOGLE_REDIRECTURI'],
+            'clientId'     => $_SERVER['OAUTH2_GOOGLE_CLIENTID'],
+            'clientSecret' => $_SERVER['OAUTH2_GOOGLE_CLIENTSECRET'],
+            'redirectUri'  => $_SERVER['OAUTH2_GOOGLE_REDIRECTURI'],
             // Enable this to restrict authentication to users at the listed domain:
-            // 'hostedDomain' => $_ENV['OAUTH2_GOOGLE_HOSTEDDOMAIN'],
+            // 'hostedDomain' => $_SERVER['OAUTH2_GOOGLE_HOSTEDDOMAIN'],
         ],
     ],
      */
