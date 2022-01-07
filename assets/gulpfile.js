f62cda9d9170d014534c052241602e3fab0a272e
Matthew Weier O'Phinney <matthew@weierophinney.net>
2022-01-07 11:46:05 -0600

    fix: prepare CSS assets as screen and print assets separately
    - Allows two instead of three tasks to build all CSS.
    - Properly names print css file print.min.css.
    - Properly provides sourcemaps for minified CSS and JS.

--- assets/gulpfile.js
+++ assets/gulpfile.js
@@ -1,69 +1,71 @@
 'use strict';
 
-const concat = require('gulp-concat');
-const del    = require('del');
-const gulp   = require('gulp');
-const minify = require('gulp-clean-css');
-const rename = require('gulp-rename');
-const sass   = require('gulp-sass')(require('node-sass'));
-const uglify = require('gulp-uglify');
+const concat     = require('gulp-concat');
+const del        = require('del');
+const gulp       = require('gulp');
+const merge      = require('gulp-merge');
+const minify     = require('gulp-clean-css');
+const rename     = require('gulp-rename');
+const sass       = require('gulp-sass')(require('node-sass'));
+const sourcemaps = require('gulp-sourcemaps');
+const uglify     = require('gulp-uglify');
 
 // CSS
 
-gulp.task('css-process-sass', () => {
-    return gulp.src([
-        'css/bootstrap.scss',
-        'css/toggle-bootstrap-print.scss',
-    ])
-    .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('build/css'));
+gulp.task('css-screen', () => {
+    let sassCss = gulp.src('css/bootstrap.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass().on('error', sass.logError));
+
+    let extraCss = gulp.src('node_modules/prism-solarized-dark/prism-solarizeddark.css')
+        .pipe(sourcemaps.init());
+
+    return merge(sassCss, extraCss)
+        .pipe(concat('styles.css'))
+        .pipe(minify())
+        .pipe(rename((file) => {
+            file.extname = '.min.css';
+        }))
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest('build/css'));
 });
 
-gulp.task('css-global-styles', () => {
-    return gulp.src([
-        'build/css/bootstrap.css',
-        'node_modules/prism-solarized-dark/prism-solarizeddark.css',
-    ])
-    .pipe(concat('styles.css'))
-    .pipe(gulp.dest('build/css/'));
-});
-
-gulp.task('css-minify', () => {
-    return gulp.src(
-        [
-            'build/css/styles.css',
-            'build/css/toggle-bootstrap-print.css',
-        ],
-        { sourcemaps: true }
-    )
-    .pipe(minify())
-    .pipe(rename((file) => {
-        file.extname = '.min.css';
-    }))
-    .pipe(gulp.dest('build/css'));
+gulp.task('css-print', () => {
+    return gulp.src('css/toggle-bootstrap-print.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass().on('error', sass.logError))
+        .pipe(minify())
+        .pipe(rename((file) => {
+            file.stem    = 'print';
+            file.extname = '.min.css';
+        }))
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest('build/css'));
 });
 
 gulp.task('css-clean', () => {
     return del(['build/css/*.*']);
 });
 
-gulp.task('css', gulp.series('css-process-sass', 'css-global-styles', 'css-minify'));
+gulp.task('css', gulp.series('css-screen', 'css-print'));
 
 // JS
 
 gulp.task('js-global', () => {
     return gulp.src(
-        [
-          'node_modules/jquery/dist/jquery.js',
-          'node_modules/autocomplete.js/dist/autocomplete.jquery.js',
-          'node_modules/bootstrap/dist/js/bootstrap.js',
-          'js/search.js'
-        ],
-        { sourcemaps: true }
-    )
-    .pipe(concat('site.min.js'))
-    .pipe(uglify())
-    .pipe(gulp.dest('build/js'));
+            [
+            'node_modules/jquery/dist/jquery.js',
+            'node_modules/autocomplete.js/dist/autocomplete.jquery.js',
+            'node_modules/bootstrap/dist/js/bootstrap.js',
+            'js/search.js'
+            ],
+            { sourcemaps: true }
+        )
+        .pipe(sourcemaps.init())
+        .pipe(concat('site.min.js'))
+        .pipe(uglify())
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest('build/js'));
 });
 
 gulp.task('js-extra', () => {
