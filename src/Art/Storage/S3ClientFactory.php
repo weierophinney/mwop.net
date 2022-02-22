009fa0e99cc5467dfc3a087b158552f66045f864
Matthew Weier O'Phinney <matthew@weierophinney.net>
2022-02-22 12:28:01 -0600

    fix: use CurlHandler instead of default CurlMultiHandler with AWS SDK
    Because the client is _stateful_, the first attempt to fetch an image sets the curl multi handle resource in the instance.
    Once set, parallel requests that come in are blocked from working, as its already been initialized.
    
    Under Swoole, non-multi-handle requests are run in coroutines, so they already benefit from parallelization.
    
    As such, the solution is to use the CurlHandler instead of the CurlMultiHandler (which is the default for a guzzle client instance).

--- src/Art/Storage/S3ClientFactory.php
+++ src/Art/Storage/S3ClientFactory.php
@@ -4,7 +4,11 @@ declare(strict_types=1);
 
 namespace Mwop\Art\Storage;
 
+use Aws\Handler\GuzzleV6\GuzzleHandler;
 use Aws\S3\S3Client;
+use GuzzleHttp\Client;
+use GuzzleHttp\Handler\CurlHandler;
+use GuzzleHttp\HandlerStack;
 use Psr\Container\ContainerInterface;
 
 class S3ClientFactory
@@ -12,6 +16,10 @@ class S3ClientFactory
     public function __invoke(ContainerInterface $container): S3Client
     {
         $config = $container->get('config-art');
+
+        $httpClient = new Client([
+            'handler' => HandlerStack::create(new CurlHandler()),
+        ]);
 
         return new S3Client([
             'version'     => 'latest',
@@ -21,6 +29,7 @@ class S3ClientFactory
                 'key'    => $config['storage']['key'],
                 'secret' => $config['storage']['secret'],
             ],
+            'http_handler' => new GuzzleHandler($httpClient),
         ]);
     }
 }
