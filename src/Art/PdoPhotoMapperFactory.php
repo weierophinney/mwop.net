c66b9f99e132db718723ceaf1875f0a75443c494
Matthew Weier O'Phinney <matthew@weierophinney.net>
2022-03-15 15:02:04 -0500

    fix: enable WAL mode in database, and introduce timeout

--- src/Art/PdoPhotoMapperFactory.php
+++ src/Art/PdoPhotoMapperFactory.php
@@ -14,6 +14,9 @@ class PdoPhotoMapperFactory
         $config = $container->get('config-art');
         $pdo    = new PDO(dsn: $config['db']['dsn']);
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        // Timeout after 3s to prevent database locks
+        $pdo->setAttribute(PDO::ATTR_TIMEOUT, 3);
+        $pdo->exec('PRAGMA journal_mode=WAL;');
 
         return new PdoPhotoMapper($pdo);
     }
