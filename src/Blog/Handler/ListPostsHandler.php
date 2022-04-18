e8cec3f6404e755bd56a9233f8486205f4bc1990
Matthew Weier O'Phinney <matthew@weierophinney.net>
2022-04-18 09:13:21 -0500

    fix: fix mobile layout with pagination and blog pages

--- src/Blog/Handler/ListPostsHandler.php
+++ src/Blog/Handler/ListPostsHandler.php
@@ -37,6 +37,7 @@ class ListPostsHandler implements RequestHandlerInterface
         $posts = $tag ? $this->mapper->fetchAllByTag($tag) : $this->mapper->fetchAll();
 
         $posts->setItemCountPerPage(10);
+        $posts->setPageRange(7);
 
         // If the requested page is later than the last, redirect to the last
         if (count($posts) && $page > count($posts)) {
