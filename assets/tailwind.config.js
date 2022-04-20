b9f180b53ac1bccb82ab74f8f3ca2e58a32b1a5e
Matthew Weier O'Phinney <matthew@weierophinney.net>
2022-04-20 15:14:12 -0500

    refactor: theme definition
    Adds colors to theme, and updates all places using color to use the theme.

--- assets/tailwind.config.js
+++ assets/tailwind.config.js
@@ -1,13 +1,25 @@
+const colors = require('tailwindcss/colors');
+
 module.exports = {
   content: ["../src/**/*.phtml", "../templates/**/*.phtml"],
   theme: {
     extend: {
-      listStyleType: {
-        none: 'none',
-        disc: 'disc',
-        decimal: 'decimal',
-        dash: '"— "',
-      }
+        colors: {
+            mwop: {
+                bg: colors.neutral[800],
+                nav: colors.neutral[700],
+                dark: colors.neutral[500],
+                soft: colors.neutral[400],
+                light: colors.neutral[300],
+                fg: colors.neutral[100],
+            },
+            action: {
+                bg: colors.emerald[500],
+                border: colors.emerald[800],
+                active: colors.emerald[200],
+                highlight: colors.emerald[900],
+            },
+        },
     },
   },
   plugins: [],
