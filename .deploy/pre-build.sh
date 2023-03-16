b549c44aaa820dce659e249c05ba097935e9da54
Matthew Weier O'Phinney <matthew@weierophinney.net>
2023-03-16 17:16:07 -0500

    refactor: updates deployment to create new shared volume
    No longer using the photodb volume, as there's a more general shared data volume.

--- .deploy/pre-build.sh
+++ .deploy/pre-build.sh
@@ -6,6 +6,6 @@ if ! docker volume ls | grep -q "mwop_net_redis";then
     docker volume create mwop_net_redis
 fi
 
-if ! docker volume ls | grep -q "mwop_net_photodb";then
-    docker volume create mwop_net_photodb
+if ! docker volume ls | grep -q "mwop_net_shared_data";then
+    docker volume create mwop_net_shared_data
 fi
