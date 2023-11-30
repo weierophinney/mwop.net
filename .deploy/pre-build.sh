61bb7d75675a5d0068461cedbae4114e5586cd09
Matthew Weier O'Phinney <matthew@weierophinney.net>
2023-11-30 14:48:32 -0600

    fix: update pre-deploy script to reflect changes
    - Create a volume for the ZendHQ DB
    - Remove creation of the worker Dockerfile; no longer used

--- .deploy/pre-build.sh
+++ .deploy/pre-build.sh
@@ -10,6 +10,10 @@ if ! docker volume ls | grep -q "mwop_net_shared_data";then
     docker volume create mwop_net_shared_data
 fi
 
-for dockerfile in nginx php worker; do
+if ! docker volume ls | grep -q "mwop_net_zendhq_db";then
+    docker volume create mwop_net_zendhq_db
+fi
+
+for dockerfile in nginx php; do
     awk -v "template=$(cat "./.docker/${dockerfile}.prod-template.Dockerfile")" "{sub(/## TEMPLATED ##/,template)}1" "./.docker/${dockerfile}.Dockerfile" > "./.docker/${dockerfile}.prod.Dockerfile"
 done
