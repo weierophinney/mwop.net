0b3a03fdf0400d97068e71ac41288ea5566d13e6
Matthew Weier O'Phinney <matthew@weierophinney.net>
2023-12-01 09:39:03 -0600

    deploy: switch to make targets inside of pre-build

--- .deploy/pre-build.sh
+++ .deploy/pre-build.sh
@@ -14,6 +14,5 @@ if ! docker volume ls | grep -q "mwop_net_zendhq_db";then
     docker volume create mwop_net_zendhq_db
 fi
 
-for dockerfile in nginx php; do
-    awk -v "template=$(cat "./.docker/${dockerfile}.prod-template.Dockerfile")" "{sub(/## TEMPLATED ##/,template)}1" "./.docker/${dockerfile}.Dockerfile" > "./.docker/${dockerfile}.prod.Dockerfile"
-done
+make prod-dockerfiles
+make pull-images
