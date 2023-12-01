6a346c195ec3285131d0ea4ca4568f86f6d40887
Matthew Weier O'Phinney <matthew@weierophinney.net>
2023-12-01 11:17:01 -0600

    fix: fix path in pull-images script
    was missing a slash

--- bin/pull-images.sh
+++ bin/pull-images.sh
@@ -11,7 +11,7 @@ do
     docker pull "${image}"
 done
 
-for dockerfile in "${app_path}/.docker/nginx.Dockerfile" "${app_path}.docker/php.Dockerfile"; do
+for dockerfile in "${app_path}/.docker/nginx.Dockerfile" "${app_path}/.docker/php.Dockerfile"; do
     grep -P "^FROM (\S+)" < "${dockerfile}" | sed -r "s/^FROM (\S+).*$/\1/" | while IFS= read -r image
     do
         echo "Pulling ${image}..."
